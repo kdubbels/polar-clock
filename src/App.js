0016f228581d3347bf99b42f723c7ad61ef372c4
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2020-12-13 09:01:58 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import {useReducer, useEffect, useCallback} from 'react';
-import {arc} from 'd3-shape';
 import './App.css';
 import './index.css';
 
@@ -12,7 +11,6 @@ const initialState = {
   date: '',
   month: ''
 };
-
 
 function getDay() {
   const now = new Date();
@@ -62,29 +60,20 @@ function App() {
     return () => clearInterval(interval);
   }, [setDate]);
 
-
-  useEffect(() => {
-    console.log(state)
-  }, [state]);
-
-  // For reference on stroke-dasharray, this was an incredible resource, even
-  // though I'd consider myself "advanced" on the topic. This made me realize
-  // I definitely have a lot yet to learn.
+  // For reference on stroke-dasharray, I made heavy use of this article.
+  // I've used stroke-dasharray previously on quite a few projects but this
+  // finally made it "click". No more guesswork.
   // 
   // https://codepen.io/xgad/post/svg-radial-progress-meters
 
   return (
     <div className="App">
-      {hours && (
-        <pre>
-            <span className='hours'>{hours}</span>:<span className="minutes">{minutes < 10 ? `0${minutes}` : minutes}</span>:<span className='seconds'>{seconds < 10 ? `0${seconds}` : seconds}</span> {hours < 12 ? 'AM' : 'PM'} | <span className="day">{date}</span>/<span className="month">{month}</span>
-        </pre>
-      )}
       <div>
-        <svg width="420" height="420" viewBox="0 0 420 420">
+      {hours && (
+        <svg width="500" height="500" viewBox="0 0 500 500">
           <filter id="blurMe" filterUnits="userSpaceOnUse">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur5"/>
-            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur10"/>
+            <feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur5"/>
+            <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur10"/>
             <feMerge result="blur-merged">
               <feMergeNode in="blur10"/>
             </feMerge>
@@ -99,22 +88,18 @@ function App() {
               <feMergeNode in="SourceGraphic"/>
             </feMerge>
           </filter>
-
-            <circle className="month" cy="50%" cx="50%" r="25" strokeDasharray={`${157 * (month/12) }, 157`} filter="url(#blurMe)"></circle>
-            {/* <circle className="month" cy="50%" cx="50%" r="25" strokeDasharray={`${157 * (month/12) }, 157`}></circle> */}
-
-            <circle className="day" cy="50%" cx="50%" r="65" strokeDasharray={`${408.4 * (date/31) }, 408.4`} filter="url(#blurMe)"></circle>
-            {/* <circle className="day" cy="50%" cx="50%" r="65" strokeDasharray={`${408.4 * (date/31) }, 408.4`}></circle> */}
-
-            <circle className="hours" cy="50%" cx="50%" r="105" strokeDasharray={`${659.73 * (hours/12) }, 659.73`} filter="url(#blurMe)"></circle>
-            {/* <circle className="hours" cy="50%" cx="50%" r="105" strokeDasharray={`${659.73 * (hours/12) }, 659.73`}></circle> */}
-
-            <circle className="minutes" cy="50%" cx="50%" r="145" strokeDasharray={`${911.061 * (minutes/60) }, 911.061`} filter="url(#blurMe)"></circle>
-            {/* <circle className="minutes" cy="50%" cx="50%" r="145" strokeDasharray={`${911.061 * (minutes/60) }, 911.061`}></circle> */}
-
-            <circle className="seconds" cy="50%" cx="50%" r="185" strokeDasharray={`${1162.38 * (seconds/60) }, 1162.38`} strokeWidth="30" filter="url(#blurMe)"></circle>    
-            {/* <circle className="seconds" cy="50%" cx="50%" r="185" strokeDasharray={`${1162.38 * (seconds/60) }, 1162.38`} strokeWidth="30"></circle>     */}
+            <text x="50%" y="10%" filter="url(#blurMe)">
+              <tspan className="hours">{hours}</tspan><tspan className="blink">:</tspan><tspan className="minutes">{minutes < 10 ? `0${minutes}` : minutes}</tspan><tspan className="blink">:</tspan><tspan className="seconds">{seconds < 10 ? `0${seconds}` : seconds}</tspan> <tspan className="white-text">{hours < 12 ? 'AM' : 'PM'}</tspan> <tspan className="blink">|</tspan> <tspan className="day">{date}</tspan><tspan className="blink">/</tspan><tspan className="month">{month}</tspan>
+            </text>
+            <g className="clock" style={{transform: "translate(0, 45px)  rotate(-90deg)"}}>
+              <circle className="month" cy="50%" cx="50%" r="25" strokeDasharray={`${157 * (month/12) }, 157`} filter="url(#blurMe)"></circle>
+              <circle className="day" cy="50%" cx="50%" r="65" strokeDasharray={`${408.4 * (date/31) }, 408.4`} filter="url(#blurMe)"></circle>
+              <circle className="hours" cy="50%" cx="50%" r="105" strokeDasharray={`${659.73 * (hours/12) }, 659.73`} filter="url(#blurMe)"></circle>
+              <circle className="minutes" cy="50%" cx="50%" r="145" strokeDasharray={`${911.061 * (minutes/60) }, 911.061`} filter="url(#blurMe)"></circle>
+              <circle className="seconds" cy="50%" cx="50%" r="185" strokeDasharray={`${1162.38 * (seconds/60) }, 1162.38`} strokeWidth="30" filter="url(#blurMe)"></circle>    
+            </g>
         </svg>
+        )}
       </div>
     </div>
   );
